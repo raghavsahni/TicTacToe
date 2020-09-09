24eb3639511fafd5700878f245ab9f8004945517
Raghav Sahni <32719066+raghavsahni@users.noreply.github.com>
2020-09-09 22:59:54 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SquareComponent } from './square/square.component';
+import { BoardComponent } from './board/board.component';
+import { CountdownModule } from 'ngx-countdown';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SquareComponent,
+    BoardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CountdownModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
